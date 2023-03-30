2683f5db7a2b1a57a20a2b7402bc9eeca3220e74
nico <nico.biagi@gmail.com>
2023-03-30 18:40:13 +0200

    fixing stimuli

--- VST/shuffled_stimuli.js
+++ VST/shuffled_stimuli.js
@@ -1,402 +1,402 @@
 var shuffled_stimuli = [
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,zxcv",
+    "stimuli": "test,test,test,zxcv",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,asdf",
+    "stimuli": "zxcv,zxcv,zxcv,asdf",
     "correct_response": "z"
   },
   {
     "block": "neutral",
-    "stimuli": "test ,test ,test ,practice",
+    "stimuli": "test,test,test,practice",
     "correct_response": "m"
   },
   {
     "block": "neutral",
-    "stimuli": "zxcv ,zxcv ,zxcv ,test",
+    "stimuli": "zxcv,zxcv,zxcv,test",
     "correct_response": "m"
   }
 ]
